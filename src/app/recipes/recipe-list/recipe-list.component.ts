658dd0f2b0c6f7ed8493f2fce71f72bc17608127
Stephen Asamoah <sasamoah@yahoo.com>
2018-01-14 12:40:23 -0500

    Added Directives

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { EventEmitter } from '@angular/core';
+import { Output } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,7 +10,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [
+ @Output() recipeWasSelected = new EventEmitter<Recipe>()
+
+@Input()  recipes: Recipe[] = [
     new Recipe('A Test Recipe 1', 'This is simple recipe', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
     new Recipe('A Recipe 2', 'This is simple recipe 2', 'https://www.ndtv.com/cooks/images/kerala.chicken.roast%20%281%29.jpg'),
     new Recipe('A Recipe 3', 'This is 3 sample', 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--488691_11.jpg?itok=ExaTspz1')
@@ -20,5 +24,7 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
